3bc03926fece81065c4fe7cba09433a1edf7f270
Nick Gospodinov <nikogosp@protonmail.com>
2026-02-07 12:29:59 -0800

    feat: AP-001 - Switch to development build with expo-dev-client

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -7,7 +7,7 @@ export default function NotFoundScreen() {
       <Stack.Screen options={{ title: 'Oops!' }} />
       <View className="flex-1 bg-slate-950 items-center justify-center p-5">
         <Text className="text-xl font-semibold text-white">Page not found</Text>
-        <Link href="/" className="mt-4">
+        <Link href="/analysis" className="mt-4">
           <Text className="text-teal-400 text-base">Go to home screen</Text>
         </Link>
       </View>
